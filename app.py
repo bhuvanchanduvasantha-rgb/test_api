71773c543b72392d38bc1aff68469d33e2c20238
bhuvanchanduvasantha-rgb <bhuvanchanduvasantha@gmail.com>
2026-04-01 11:36:44 +0530

    Fix typo in success status message

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def get_albums():
     response = requests.request("GET", url, headers=headers, data=payload)
     return jsonify({
         "data": response.json(),
-        "status": "successfull message",
+        "status": "successfull ",
         "status_code": 200
     })
 
